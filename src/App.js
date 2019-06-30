0e6d749ffc7b44781d0267a3d506e9ba9cab1272
Tommy Scribble <tommy@scribbledesign.co.uk>
2019-06-30 18:52:36 +0100

    add width prop to account component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,7 @@ import React from 'react';
 import Icon from './components/icons/InTouchIcons';
 
 const App = props => {
-	const style = {
-		width: '150px',
-	};
-
-	return (
-		<div style={style}>
-			<Icon iconName={'accountDark'} fill="green" />
-		</div>
-	);
+	return <Icon iconName={'accountDark'} fill="green" width={'150px'} />;
 };
 
 export default App;
