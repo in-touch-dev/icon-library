a851338259aba6f5d0e396fe3bd67f1ed931bd5c
Tommy Scribble <tommy@scribbledesign.co.uk>
2019-06-30 18:34:27 +0100

    add fill prop

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from 'react';
 import Icon from './components/icons/index';
 
 const App = props => {
-	return <Icon iconName={'networkDark'} />;
+	const style = {
+		width: '150px',
+	};
+
+	return (
+		<div style={style}>
+			<Icon iconName={'messageDark'} fill="green" />
+		</div>
+	);
 };
 
 export default App;
