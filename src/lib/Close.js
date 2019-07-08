89fd4328e0460c5f99bd7c0e5770f3ac8912e782
liam riley <liam.riley@intouchnetworks.com>
2019-07-08 16:20:20 +0100

    Close icon changed

--- src/lib/Close.js
+++ src/lib/Close.js
@@ -8,19 +8,16 @@ const Close = props => {
 
 	return (
 		<svg
-			className={`icon-itn resources-icon ${props.svgClass}`}
+			className={`icon-itn message-icon ${props.svgClass}`}
 			style={style}
 			xmlns="http://www.w3.org/2000/svg"
 			xmlnsXlink="http://www.w3.org/1999/xlink"
-			viewBox="19 15 22 30"
+			viewBox="0 0 212.982 212.982"
 			aria-hidden={props.ariaHidden}
 			focusable={props.focusable}
 		>
-			<defs>
-				<path id="A" d="M0 0h59.7v59.7H0z" />
-			</defs>
 			<path
-				d="M30 33.8l4.7 8.4h6.2l-8-12.8 7.5-11.8h-5.9l-4.5 8-4.4-8h-6.1l7.5 12-7.9 12.6h6z"
+				d="M131.804,106.491l75.936-75.936c6.99-6.99,6.99-18.323,0-25.312   c-6.99-6.99-18.322-6.99-25.312,0l-75.937,75.937L30.554,5.242c-6.99-6.99-18.322-6.99-25.312,0c-6.989,6.99-6.989,18.323,0,25.312   l75.937,75.936L5.242,182.427c-6.989,6.99-6.989,18.323,0,25.312c6.99,6.99,18.322,6.99,25.312,0l75.937-75.937l75.937,75.937   c6.989,6.99,18.322,6.99,25.312,0c6.99-6.99,6.99-18.322,0-25.312L131.804,106.491z"
 				fill={props.fill}
 			/>
 		</svg>
@@ -37,6 +34,7 @@ Close.propTypes = {
 
 Close.defaultProps = {
 	ariaHidden: 'true',
+	svgClass: '',
 	focusable: 'false',
 	fill: '#30353a',
 };
