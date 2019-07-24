ccd4f9c8cd979378f98caeadce9aba1731db7b12
Tommy <tommy.ollerenshaw@intouchnetworks.com>
2019-07-24 11:53:16 +0100

    add all icon componets to a obj

---
+++ src/lib/IconObj.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Account from './Account';
+import Message from './Message';
+import Network from './Network';
+import Profile from './Profile';
+import Resources from './Resources';
+import Work from './Work';
+import Settings from './Settings';
+import Close from './Close';
+import Play from './Play';
+import Basket from './Basket';
+
+const IconObj = {
+	account: <Account />,
+	message: <Message />,
+	network: <Network />,
+	profile: <Profile />,
+	resources: <Resources />,
+	work: <Work />,
+	settings: <Settings />,
+	close: <Close />,
+	play: <Play />,
+	basket: <Basket />,
+};
+
+export default IconObj;
